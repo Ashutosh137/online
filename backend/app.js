0dc977e16efca384510522757a8872f346f03975
ashutosh <mr.luckysharma7@gmail.com>
2024-06-14 13:55:52 +0530

    nkkk

--- backend/app.js
+++ backend/app.js
@@ -2,8 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const { login } = require("../routes/login,js");
-const { register } = require("../routes/login,js");
+const { login } = require("./routes/login,js");
+const { register } = require("./routes/login,js");
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -18,7 +18,7 @@ app.use(cors({ origin: "*" })); // Consider a more restrictive origin for produc
 
 // Root route with potential optimization based on use case
 app.get("/", async (req, res) => {
-  return res.json("welcome");
+   res.json("welcome");
 });
 app.post("/login", login);
 app.post("/signup", register);
